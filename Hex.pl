e8e1d602cb3dde599d38da98d05dc16259281322
Oscar Carlsson <oscar.carlsson.87@gmail.com>
2013-03-03 14:58:48 +0100

    Converted tabs to spaces

--- Hex.pl
+++ Hex.pl
@@ -46,10 +46,10 @@ print $sock "USER $bot_nick $server * :$bot_nick\r\n";
 
 while(my $input = <$sock>){
    if($input =~ /004/){
-	last;
+    last;
     }
     elsif($input =~ /433/){
-	die "Nickname already in use\n";
+    die "Nickname already in use\n";
     }
 }
 
@@ -73,29 +73,29 @@ while(my $input = <$sock>){
     
     # If its a ping send a pong
     if($in_mask eq "PING"){
-	print $sock "PONG $in_type\r\n";
+    print $sock "PONG $in_type\r\n";
     }
     elsif($in_type eq "JOIN" || $in_type eq "PART" || $in_type eq "QUIT"){
         update_last_seen($in_mask, $in_type);
     }
     elsif($in_type eq "PRIVMSG"){
-	
-	# Parse out the command and remove the ":"
-	(my $command, my $arguments) = split(/ /, $in_text, 2);
-	$command = substr $command, 1;
+    
+    # Parse out the command and remove the ":"
+    (my $command, my $arguments) = split(/ /, $in_text, 2);
+    $command = substr $command, 1;
 
-	# Find the nick
-	$in_mask =~ /^\:(.*)\!/;
-	my $in_nick = $1;
+    # Find the nick
+    $in_mask =~ /^\:(.*)\!/;
+    my $in_nick = $1;
 
-	# Private message to bot
-	if($in_channel eq $bot_nick){
-	    priv_message($command, $arguments, $in_mask, $in_nick);
-	}
-	# Messages in channels
-	else{
-	    channel_message($in_channel, $in_nick, $in_text);
- 	}# End of messages in channels
+    # Private message to bot
+    if($in_channel eq $bot_nick){
+        priv_message($command, $arguments, $in_mask, $in_nick);
+    }
+    # Messages in channels
+    else{
+        channel_message($in_channel, $in_nick, $in_text);
+    }# End of messages in channels
     }# End of "PRIVMSG"
 }
 
@@ -106,13 +106,13 @@ sub priv_message{
     my $in_nick = shift;
 
     if($in_mask eq $admin_mask){ # If admin tells me
-	if(priv_from_admin($command, $arguments) == 0){
-	    priv_from_other($command, $arguments, $in_mask, $in_nick);
-	}
-	
+    if(priv_from_admin($command, $arguments) == 0){
+        priv_from_other($command, $arguments, $in_mask, $in_nick);
+    }
+    
     }# End of messages from admin
     else{ # Other people tell me
-	priv_from_other($command, $arguments, $in_mask, $in_nick);
+    priv_from_other($command, $arguments, $in_mask, $in_nick);
     }# End of messages from other people
 }
 
@@ -122,89 +122,89 @@ sub priv_from_admin{
     my $arguments = shift;
 
     if($command eq "!join"){
-	
-	my @tojoin = split(/ /, $arguments);
+    
+    my @tojoin = split(/ /, $arguments);
 
-	foreach my $channel (@tojoin){
-	    my $exists = 0; 
-	    my $nrOfChannels = $#channels;
+    foreach my $channel (@tojoin){
+        my $exists = 0; 
+        my $nrOfChannels = $#channels;
 
-	    for(my $i = 0; $exists == 0 && $i <= $nrOfChannels; $i++){
-		
-		if($channel eq $channels[$i]){
-		    $exists = 1;
-		}
-	    }
-	    
-	    if($exists){
-		print $sock "PRIVMSG $admin_nick :Already in $channel\r\n";
-	    }
-	    else{
-		print $sock "JOIN $channel\r\n";
-		push(@channels, $channel);
-	    }
-	}
+        for(my $i = 0; $exists == 0 && $i <= $nrOfChannels; $i++){
+        
+        if($channel eq $channels[$i]){
+            $exists = 1;
+        }
+        }
+        
+        if($exists){
+        print $sock "PRIVMSG $admin_nick :Already in $channel\r\n";
+        }
+        else{
+        print $sock "JOIN $channel\r\n";
+        push(@channels, $channel);
+        }
+    }
     }# End of !join
     elsif($command eq "!part"){
-	
-	my @topart = split(/ /, $arguments);
+    
+    my @topart = split(/ /, $arguments);
 
-	foreach my $channel (@topart){
-	    my $found = 0;
-	    for(my $i = 0; $found == 0 && $i <= $#channels; $i++){
+    foreach my $channel (@topart){
+        my $found = 0;
+        for(my $i = 0; $found == 0 && $i <= $#channels; $i++){
 
-		if($channels[$i] eq $channel){
-		    print $sock "PART $channel\r\n";
-		    splice(@channels, $i, 1);
-		    $found = 1;
-		}
-	    }
-	}
-	
+        if($channels[$i] eq $channel){
+            print $sock "PART $channel\r\n";
+            splice(@channels, $i, 1);
+            $found = 1;
+        }
+        }
+    }
+    
     }# End of !part
     elsif($command eq "!where"){
-	foreach(@channels){
-	    print $sock "PRIVMSG $admin_nick :$_\r\n";
-	}
+    foreach(@channels){
+        print $sock "PRIVMSG $admin_nick :$_\r\n";
+    }
     }# End of !where
     elsif($command eq "!say"){
-	(my $channel, my $text) = split(/ /, $arguments, 2); 
-	print $sock "PRIVMSG $channel :$text\r\n";
+    (my $channel, my $text) = split(/ /, $arguments, 2); 
+    print $sock "PRIVMSG $channel :$text\r\n";
     }# End of !say
     elsif($command eq "!allow"){
-	if($arguments eq "spotify"){
-	    $allow_spotify = 1;
-	}
-	elsif($arguments eq "youtube"){
-	    $allow_youtube = 1;
-	}
-	elsif($arguments eq "new"){
-	    $allow_news = 1;
-	}
+    if($arguments eq "spotify"){
+        $allow_spotify = 1;
+    }
+    elsif($arguments eq "youtube"){
+        $allow_youtube = 1;
+    }
+    elsif($arguments eq "new"){
+        $allow_news = 1;
+    }
     }# End of !allow
     elsif($command eq "!disallow"){
-	if($arguments eq "spotify"){
-	    $allow_spotify = 0;
-	}
-	elsif($arguments eq "youtube"){
-	    $allow_youtube = 0;
-	}
-	elsif($arguments eq "new"){
-	    $allow_news = 0;
-	}
+    if($arguments eq "spotify"){
+        $allow_spotify = 0;
+    }
+    elsif($arguments eq "youtube"){
+        $allow_youtube = 0;
+    }
+    elsif($arguments eq "new"){
+        $allow_news = 0;
+    }
     }# End of disallow
     elsif($command eq "!reload"){
-	if($arguments eq "commands"){
-	    delete $INC{'commands.pm'};
-	    require commands;
-	}
-	elsif($arguments eq "links"){
-	    delete $INC{'linkparsing.pm'};
-	    require linkparsing;
-	}
+    if($arguments eq "commands"){
+        delete $INC{'commands.pm'};
+        require commands;
+    }
+    elsif($arguments eq "links"){
+        delete $INC{'linkparsing.pm'};
+        require linkparsing;
+    }
     }
     else{
-	return 0;
+    return 0;
     }
 
     return 1;
@@ -217,20 +217,20 @@ sub priv_from_other{
     my $in_nick = shift;
 
     if($command eq "!admin"){
-	if($arguments eq $admin_password){
-	    $admin_nick = $in_nick;
-	    $admin_mask = $in_mask;
-	    print $sock "PRIVMSG $admin_nick :You are now admin\r\n";
-	}
-	else{
-	    print $sock "PRIVMSG $admin_nick :$in_nick tried to guess the admin password with $arguments\r\n";
-	}
-    }
-    elsif(lc($command) eq "sten" || lc($command) eq "sax" || lc($command) eq "påse"){
-	playRPS($command, $in_nick);
+    if($arguments eq $admin_password){
+        $admin_nick = $in_nick;
+        $admin_mask = $in_mask;
+        print $sock "PRIVMSG $admin_nick :You are now admin\r\n";
     }
     else{
-	print $sock "PRIVMSG $admin_nick :$in_nick told me $command $arguments\r\n";
+        print $sock "PRIVMSG $admin_nick :$in_nick tried to guess the admin password with $arguments\r\n";
+    }
+    }
+    elsif(lc($command) eq "sten" || lc($command) eq "sax" || lc($command) eq "påse"){
+    playRPS($command, $in_nick);
+    }
+    else{
+    print $sock "PRIVMSG $admin_nick :$in_nick told me $command $arguments\r\n";
     }
 }
 
@@ -240,10 +240,10 @@ sub channel_message{
     my $in_text = shift;
 
     if(substr($in_text, 1, 1) eq "!"){
-	parse_command($sock, $in_channel, $in_nick, $in_text);
+    parse_command($sock, $in_channel, $in_nick, $in_text);
     }
     else{
-	parse_links($sock, $in_channel, $in_text);
+    parse_links($sock, $in_channel, $in_text);
     }    
 }    
 sub playRPS{
@@ -251,37 +251,37 @@ sub playRPS{
     my $nick = shift;
 
     if($RPS::phase){
-	if($RPS::player1_name eq $nick){
-	    print $sock "PRIVMSG $nick :Du kan inte spela mot dig själv\r\n";
- 	}
-	else{
-	    $RPS::player2_name = $nick;
-	    $RPS::player2_choice = $choice;
-	    $RPS::phase = 0;
-	    my $winner = RPS::play();
-	    
-	     my $sth = $dbh->prepare("INSERT INTO rps VALUES (?, ?, ?, ?, ?, ?)");
+    if($RPS::player1_name eq $nick){
+        print $sock "PRIVMSG $nick :Du kan inte spela mot dig själv\r\n";
+    }
+    else{
+        $RPS::player2_name = $nick;
+        $RPS::player2_choice = $choice;
+        $RPS::phase = 0;
+        my $winner = RPS::play();
+        
+         my $sth = $dbh->prepare("INSERT INTO rps VALUES (?, ?, ?, ?, ?, ?)");
 
-	    if($winner == 1){
-		$sth->execute($RPS::player1_name, $RPS::player1_choice, $RPS::player2_name, $RPS::player2_choice, $RPS::player1_name, $RPS::player2_name);
-		print $sock "PRIVMSG $RPS_channel :$RPS::player1_name vs $RPS::player2_name: $RPS::player1_choice vs $RPS::player2_choice. $RPS::player1_name vinner!\r\n";
+        if($winner == 1){
+        $sth->execute($RPS::player1_name, $RPS::player1_choice, $RPS::player2_name, $RPS::player2_choice, $RPS::player1_name, $RPS::player2_name);
+        print $sock "PRIVMSG $RPS_channel :$RPS::player1_name vs $RPS::player2_name: $RPS::player1_choice vs $RPS::player2_choice. $RPS::player1_name vinner!\r\n";
 
-	    }
-	    elsif($winner == 2){
-		$sth->execute($RPS::player1_name, $RPS::player1_choice, $RPS::player2_name, $RPS::player2_choice, $RPS::player2_name, $RPS::player1_name);
-	 	print $sock "PRIVMSG $RPS_channel :$RPS::player1_name vs $RPS::player2_name: $RPS::player1_choice vs $RPS::player2_choice. $RPS::player2_name vinner!\r\n";
-	    } 
-	    else{
-		$sth->execute($RPS::player1_name, $RPS::player1_choice, $RPS::player2_name, $RPS::player2_choice, "tie", "tie");
-		print $sock "PRIVMSG $RPS_channel :$RPS::player1_name vs $RPS::player2_name: $choice vs $choice. Ingen vinner\r\n";
-	    }
-	}
+        }
+        elsif($winner == 2){
+        $sth->execute($RPS::player1_name, $RPS::player1_choice, $RPS::player2_name, $RPS::player2_choice, $RPS::player2_name, $RPS::player1_name);
+        print $sock "PRIVMSG $RPS_channel :$RPS::player1_name vs $RPS::player2_name: $RPS::player1_choice vs $RPS::player2_choice. $RPS::player2_name vinner!\r\n";
+        } 
+        else{
+        $sth->execute($RPS::player1_name, $RPS::player1_choice, $RPS::player2_name, $RPS::player2_choice, "tie", "tie");
+        print $sock "PRIVMSG $RPS_channel :$RPS::player1_name vs $RPS::player2_name: $choice vs $choice. Ingen vinner\r\n";
+        }
+    }
 
     }
     else{
-	$RPS::player1_name = $nick;
-	$RPS::player1_choice = $choice;
-	$RPS::phase = 1;
+    $RPS::player1_name = $nick;
+    $RPS::player1_choice = $choice;
+    $RPS::phase = 1;
     }
 }
 
--- commands.pm
+++ commands.pm
@@ -11,7 +11,7 @@ sub parse_command{
     $command = substr $command, 1;
 
     if($command eq "!score"){
-	    score($sock, $in_channel, $arguments);
+        score($sock, $in_channel, $arguments);
     }
     elsif($command eq "!whattowatch"){
         whattowatch($sock, $in_channel);
@@ -48,64 +48,64 @@ sub score{
     my $arguments = shift;
 
     if(!defined($arguments)){
-	%points = ();
+    %points = ();
 
-	my $sth = $dbh->prepare("SELECT winner, count(winner) FROM rps WHERE winner <> 'tie' GROUP BY winner ORDER BY count(winner)");
-	$sth->execute;
-	
-	while(my @wins = $sth->fetchrow_array){
-	    $points{$wins[0]} = $wins[1];
-	}
+    my $sth = $dbh->prepare("SELECT winner, count(winner) FROM rps WHERE winner <> 'tie' GROUP BY winner ORDER BY count(winner)");
+    $sth->execute;
+    
+    while(my @wins = $sth->fetchrow_array){
+        $points{$wins[0]} = $wins[1];
+    }
 
-	$sth = $dbh->prepare("SELECT looser, count(looser) FROM rps WHERE looser <> 'tie' GROUP BY looser ORDER BY count(looser)");
-	$sth->execute;
-	while(my @losses = $sth->fetchrow_array){
-	    $points{$losses[0]}-= $losses[1];
-	}
-	
+    $sth = $dbh->prepare("SELECT looser, count(looser) FROM rps WHERE looser <> 'tie' GROUP BY looser ORDER BY count(looser)");
+    $sth->execute;
+    while(my @losses = $sth->fetchrow_array){
+        $points{$losses[0]}-= $losses[1];
+    }
+    
 
-	my @nicks = sort {$points{$b} <=> $points{$a}} keys %points;
-	foreach(@nicks){
-	    print "$_ $points{$_}\n";
-	}
-	print $sock "PRIVMSG $in_channel :$nicks[0] leder med $points{$nicks[0]} poäng\r\n";
-	print $sock "PRIVMSG $in_channel :$nicks[$#nicks] ligger sist med $points{$nicks[$#nicks]} poäng\r\n";
+    my @nicks = sort {$points{$b} <=> $points{$a}} keys %points;
+    foreach(@nicks){
+        print "$_ $points{$_}\n";
+    }
+    print $sock "PRIVMSG $in_channel :$nicks[0] leder med $points{$nicks[0]} poäng\r\n";
+    print $sock "PRIVMSG $in_channel :$nicks[$#nicks] ligger sist med $points{$nicks[$#nicks]} poäng\r\n";
 
     }
     else{
-	(my $nick1, my $nick2) = split(/ /, $arguments, 2);
-	
-	if(!defined($nick2)){
-	    my $sth = $dbh->prepare("SELECT (SELECT count(*) FROM rps WHERE winner = ?) AS wins, (SELECT count(*) FROM rps WHERE looser = ?) AS loses");
-	   
-	    $sth->execute($nick1, $nick1);
-	    @result = $sth->fetchrow_array;
-	    print $sock "PRIVMSG $in_channel :$nick1 har vunnit $result[0] ggr och förlorat $result[1] ggr\r\n";
-	}
-	else{
-	    my $sth = $dbh->prepare("SELECT COUNT(winner) from rps where winner = ? and looser = ?");
-	    $sth->execute($nick1, $nick2);
-	    my @result = $sth->fetchrow_array;
-	    my $nick1_wins = $result[0];
-	    #$sth = $dbh->prepare("SELECT count(winner) from rps where winner = ? and looser = ?");
-	    $sth->execute($nick2, $nick1);
-	    @result = $sth->fetchrow_array;
-	    if($nick1_wins > $result[0]){
-		print $sock "PRIVMSG $in_channel :$nick1 har vunnit över $nick2 $nick1_wins gånger men bara förlorat $result[0] gånger\r\n";
-	    }
-	    elsif($nick1_wins < $result[0]){
-		print $sock "PRIVMSG $in_channel :$nick2 har vunnit över $nick1 $result[0] gånger men bara förlorat $nick1_wins gånger\r\n";
-	    }
-	    else{
-		if($result[0] == 0){
-		    print $sock "PRIVMSG $in_channel :$nick1 och $nick2 har aldrig vunnit mot varandra\r\n";
-		}
-		else{
-		    print $sock "PRIVMSG $in_channel :$nick1 och $nick2 har båda vunnit $result[0] gånger mot varandra\r\n";
-		}
-	    }
-	    
-	}
+    (my $nick1, my $nick2) = split(/ /, $arguments, 2);
+    
+    if(!defined($nick2)){
+        my $sth = $dbh->prepare("SELECT (SELECT count(*) FROM rps WHERE winner = ?) AS wins, (SELECT count(*) FROM rps WHERE looser = ?) AS loses");
+       
+        $sth->execute($nick1, $nick1);
+        @result = $sth->fetchrow_array;
+        print $sock "PRIVMSG $in_channel :$nick1 har vunnit $result[0] ggr och förlorat $result[1] ggr\r\n";
+    }
+    else{
+        my $sth = $dbh->prepare("SELECT COUNT(winner) from rps where winner = ? and looser = ?");
+        $sth->execute($nick1, $nick2);
+        my @result = $sth->fetchrow_array;
+        my $nick1_wins = $result[0];
+        #$sth = $dbh->prepare("SELECT count(winner) from rps where winner = ? and looser = ?");
+        $sth->execute($nick2, $nick1);
+        @result = $sth->fetchrow_array;
+        if($nick1_wins > $result[0]){
+        print $sock "PRIVMSG $in_channel :$nick1 har vunnit över $nick2 $nick1_wins gånger men bara förlorat $result[0] gånger\r\n";
+        }
+        elsif($nick1_wins < $result[0]){
+        print $sock "PRIVMSG $in_channel :$nick2 har vunnit över $nick1 $result[0] gånger men bara förlorat $nick1_wins gånger\r\n";
+        }
+        else{
+        if($result[0] == 0){
+            print $sock "PRIVMSG $in_channel :$nick1 och $nick2 har aldrig vunnit mot varandra\r\n";
+        }
+        else{
+            print $sock "PRIVMSG $in_channel :$nick1 och $nick2 har båda vunnit $result[0] gånger mot varandra\r\n";
+        }
+        }
+        
+    }
     }
 }
 
@@ -214,10 +214,10 @@ sub addbarnen{
 
     my $sth = $dbh->prepare("insert into allabarnen (nick,joke) values (?,?);");
     if(!$sth->execute($in_nick,$joke)){
-	sqlconnect();
-	sleep(5);
-	$sth = $dbh->prepare("insert into allabarnen (nick,joke) values (?,?);");
-	$sth->execute($in_nick,$joke);
+    sqlconnect();
+    sleep(5);
+    $sth = $dbh->prepare("insert into allabarnen (nick,joke) values (?,?);");
+    $sth->execute($in_nick,$joke);
     }
 }
 
@@ -230,8 +230,8 @@ sub excuse{
     if(!$sth->execute){
         $dbh = sqlconnect();
         sleep(5);
-	$sth = $dbh->prepare("select excuse from excuses order by rand() limit 1;");
-	$sth->execute;
+    $sth = $dbh->prepare("select excuse from excuses order by rand() limit 1;");
+    $sth->execute;
     }
     
     my @answer = $sth->fetchrow_array;
@@ -244,24 +244,24 @@ sub hat{
     
     my $sth;
     if(defined $arguments){
-	$sth = $dbh->prepare("select nick, hat from hate where nick = ? order by rand() limit 1;");
-	if(!$sth->execute($arguments)){
+    $sth = $dbh->prepare("select nick, hat from hate where nick = ? order by rand() limit 1;");
+    if(!$sth->execute($arguments)){
             $dbh = sqlconnect();
             sleep(5);
         }
-	$sth = $dbh->prepare("select nick, hat from hate where nick = ? order by rand() limit 1;");
-	$sth->execute($arguments);
+    $sth = $dbh->prepare("select nick, hat from hate where nick = ? order by rand() limit 1;");
+    $sth->execute($arguments);
     }
     else{
-	$sth = $dbh->prepare("select nick, hat from hate order by rand() limit 1;");
-	
-	if(!$sth->execute){
-	    $dbh = sqlconnect();
-	    sleep(5);
-	    $sth = $dbh->prepare("select nick, hat from hate order by rand() limit 1;");
-	    $sth->execute;
-	}
-	
+    $sth = $dbh->prepare("select nick, hat from hate order by rand() limit 1;");
+    
+    if(!$sth->execute){
+        $dbh = sqlconnect();
+        sleep(5);
+        $sth = $dbh->prepare("select nick, hat from hate order by rand() limit 1;");
+        $sth->execute;
+    }
+    
     }
     my @answer = $sth->fetchrow_array;
     print $sock "PRIVMSG $in_channel :$answer[0] hatar $answer[1]\r\n";
@@ -275,11 +275,11 @@ sub jaghatar{
     my $sth = $dbh->prepare("insert into hate (nick, hat) values (?,?);");
 
     if(!$sth->execute($in_nick, $arguments)){
-	print "reconnect\n";
-	$dbh = sqlconnect();
-	sleep(5);
-	$sth = $dbh->prepare("insert into hate (nick, hat) values (?,?);");
-	$sth->execute($in_nick, $arguments)
+    print "reconnect\n";
+    $dbh = sqlconnect();
+    sleep(5);
+    $sth = $dbh->prepare("insert into hate (nick, hat) values (?,?);");
+    $sth->execute($in_nick, $arguments)
     }
 }
 
