67a737dd2d8073c4d003a5d3d53948738bfb34d5
Oscar Carlsson <oscar.carlsson.87@gmail.com>
2013-03-03 15:00:29 +0100

    Converted tabs to spaces

--- RPS.pm
+++ RPS.pm
@@ -20,42 +20,42 @@ our %losses = ();
 sub play{
 
     if($player1_choice eq "sten"){
-	if($player2_choice eq "sax"){
-	    $wins{$player1_name}++;
-	    $losses{$player2_name}++;
-	    return 1;
-	}
-	elsif($player2_choice eq "påse"){
-	    $wins{$player2_name}++;
-	    $losses{$player1_name}++;
-	    return 2;
-	}
+    if($player2_choice eq "sax"){
+        $wins{$player1_name}++;
+        $losses{$player2_name}++;
+        return 1;
+    }
+    elsif($player2_choice eq "påse"){
+        $wins{$player2_name}++;
+        $losses{$player1_name}++;
+        return 2;
+    }
     }
     elsif($player1_choice eq "sax"){
 
-	if($player2_choice eq "sten"){
-	    $wins{$player2_name}++;
-	    $losses{$player1_name}++;
-	    return 2;
-	}
-	elsif($player2_choice eq "påse"){
-	    $wins{$player1_name}++;
-	    $losses{$player2_name}++;
-	    return 1;
-	}
+    if($player2_choice eq "sten"){
+        $wins{$player2_name}++;
+        $losses{$player1_name}++;
+        return 2;
+    }
+    elsif($player2_choice eq "påse"){
+        $wins{$player1_name}++;
+        $losses{$player2_name}++;
+        return 1;
+    }
     }
     elsif($player1_choice eq "påse"){
 
-	if($player2_choice eq "sten"){
-	    $wins{$player1_name}++;
-	    $losses{$player2_name}++;
-	    return 1;
-	}
-	elsif($player2_choice eq "sax"){
-	    $wins{$player2_name}++;
-	    $losses{$player1_name}++;
-	    return 2;
-	}
+    if($player2_choice eq "sten"){
+        $wins{$player1_name}++;
+        $losses{$player2_name}++;
+        return 1;
+    }
+    elsif($player2_choice eq "sax"){
+        $wins{$player2_name}++;
+        $losses{$player1_name}++;
+        return 2;
+    }
     }
 
     return 0;
